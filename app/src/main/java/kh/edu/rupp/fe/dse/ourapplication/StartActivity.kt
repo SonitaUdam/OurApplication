8c3c2261f52ceb26f771eb66fdc53ef127835ccf
udam sonita <udamsonitaa@gmail.com>
2024-05-11 13:12:29 +0700

    SplashScreen and StartActivity

---
+++ app/src/main/java/kh/edu/rupp/fe/dse/ourapplication/StartActivity.kt
@@ -0,0 +1,11 @@
+package kh.edu.rupp.fe.dse.ourapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
