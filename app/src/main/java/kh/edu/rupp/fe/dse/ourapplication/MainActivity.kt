2a6f5b8813e545b012a0581bffa10ac9da3d7984
udam sonita <udamsonitaa@gmail.com>
2024-05-13 00:08:52 +0700

    BottomNavigation and Fragments

--- app/src/main/java/kh/edu/rupp/fe/dse/ourapplication/MainActivity.kt
+++ app/src/main/java/kh/edu/rupp/fe/dse/ourapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package kh.edu.rupp.fe.dse.ourapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kh.edu.rupp.fe.dse.ourapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setContentView(R.layout.activity_main)
+        var NavController=findNavController(R.id.fragmentContainerView)
+        var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomnav.setupWithNavController(NavController)
+
+
     }
 }
