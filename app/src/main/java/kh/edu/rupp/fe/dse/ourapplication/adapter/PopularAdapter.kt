f81e259a89379fe327e94491a230110dc66f7d3d
udam sonita <udamsonitaa@gmail.com>
2024-05-13 11:10:39 +0700

    Slider and Adding popular products

---
+++ app/src/main/java/kh/edu/rupp/fe/dse/ourapplication/adapter/PopularAdapter.kt
@@ -0,0 +1,34 @@
+package kh.edu.rupp.fe.dse.ourapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kh.edu.rupp.fe.dse.ourapplication.databinding.PopularItemBinding
+
+class PopularAdapter (private val items:List<String>, private val price:List<String>, private val image:List<Int>) : RecyclerView.Adapter<PopularAdapter.PopularViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
+        return PopularViewHolder(PopularItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
+        val item = items[position]
+        val images = image[position]
+        val price = price[position]
+        holder.bind(item,price,images)
+    }
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class PopularViewHolder(private val binding: PopularItemBinding) : RecyclerView.ViewHolder(binding.root){
+        private val imagesView = binding.imageView5
+        fun bind(item: String, price: String, images: Int) {
+            binding.ProductNamePopular.text = item
+            binding.PricePopular.text = price
+            imagesView.setImageResource(images)
+        }
+
+    }
+}
